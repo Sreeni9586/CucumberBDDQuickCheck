66670a0fe525fd83d3ec183f9528ef7734d69bb4
sreeni <srinivasreddy117@gmail.com>
2024-09-21 18:51:17 +0530

    first commit

---
+++ src/main/java/base/BaseClass.java
@@ -0,0 +1,28 @@
+package base;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import net.bytebuddy.asm.Advice;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BaseClass {
+
+    public WebDriver driver;
+
+    public WebDriver initializeDriver(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        return driver;
+    }
+    public void tearDown(){
+        if(driver!=null){
+            driver.quit();
+            driver = null;
+        }
+    }
+}
---
+++ src/test/java/Runner/CukesRunner.java
@@ -0,0 +1,20 @@
+package Runner;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(features="src/test/resources/features",
+                  glue={"stepdefs"},
+                   plugin = {"pretty","html:target/cucumber-reports","json:target/cucumber.json"},
+                    //tags= "@loginSuccess",
+                    monochrome = true,publish = true)
+
+public class CukesRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = false)
+
+    public Object[][] scenarios(){
+        return super.scenarios();
+    }
+
+}
